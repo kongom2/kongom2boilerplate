d9a5aa5cb97a206ba1679d2beddfca6a1bbd84f5
kongom2 <kongom2gmail.com>
2022-02-21 15:17:39 +0900

    Feat:회원가입 기능

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded => 이런 것들을 분석해서 가져올 수 있게 하는 것
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json => 이런 것들을 분석해서 가져올 수 있게 하는 것
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://kongom2:kongom2!@kongom2boilerplate.igpo1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
+    "mongodb+srv://kongom2:kongom2!@kongom2boilerplate.igpo1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   )
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req, res) => res.send("Hello World! ~~ 안녕하세요 ~~"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
