9a2ccc56b121fff6535efef43b60843f7fe378bc
kongom2@gmail.com <kongom2@gmail.com>
2022-02-21 15:22:32 +0900

    Feat:회원가입 기능

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World! ~~ 안녕하세요 ~~"));
+app.get("/", (req, res) => res.send("Hello World! ~~ 안녕하세요 ~~!"));
 
 app.post("/register", (req, res) => {
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
