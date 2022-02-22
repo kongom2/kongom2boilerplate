9bb648687be0d8253a73fe9bc64966167a5928b3
kongom2@gmail.com <kongom2@gmail.com>
2022-02-23 02:56:57 +0900

    Feat: Proxy로 CORS 이슈 해결하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,9 +4,9 @@ import axios from "axios";
 const LandingPage = () => {
   // 페이지에 들어오자마자 useEffect를 실행
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/hello")
-      .then((response) => console.log(response.data));
+    axios.get("/api/hello").then((response) => {
+      console.log(response);
+    });
   }, []);
 
   return <div>LandingPage</div>;
