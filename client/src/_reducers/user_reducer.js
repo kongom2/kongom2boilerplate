093be50bd33f0705e837286e3b771378f78c10d5
kongom2@gmail.com <kongom2@gmail.com>
2022-02-23 20:17:58 +0900

    Feat: Login페이지 view 작성

---
+++ client/src/_reducers/user_reducer.js
@@ -0,0 +1,11 @@
+import { LOGIN_USER } from "../_actions/types";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (state = {}, action) {
+  switch (action.type) {
+    case LOGIN_USER:
+      return { ...state, loginSuccess: action.payload };
+    default:
+      return state;
+  }
+}
