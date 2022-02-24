e4d188e323bd8bfe0782ac0085a3899a9f6ce333
kongom2@gmail.com <kongom2@gmail.com>
2022-02-24 18:12:04 +0900

    Feat: 로그아웃 페이지 view 작성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
   // 페이지에 들어오자마자 useEffect를 실행
   useEffect(() => {
     axios.get("/api/hello").then((response) => {
@@ -9,7 +12,30 @@ const LandingPage = () => {
     });
   }, []);
 
-  return <div>LandingPage</div>;
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃을 실패했습니다.");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
+    </div>
+  );
 };
 
 export default LandingPage;
